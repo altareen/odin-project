74ee0f64f4c722880a772ddad4f91cea5769f8e8
Alwin Tareen <altareen@gmail.com>
2022-12-28 17:18:32 -0500

    added rockpaperscissors

---
+++ rockpaperscissors/rockpaperscissors.js
@@ -0,0 +1,15 @@
+/**
+|-------------------------------------------------------------------------------
+| rockpaperscissors.js
+|-------------------------------------------------------------------------------
+|
+| Author:       Alwin Tareen
+| Created:      Oct 08, 2019
+| Execution:    node rockpaperscissors.js
+|
+| This program implements the Rock-Paper-Scissors game.
+|
+*/
+
+
+
